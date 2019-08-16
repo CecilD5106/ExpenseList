04eb519b929503b4d6566316f3c1c73d374a6df5
Cecil Davis <cecild5106@hotmail.com>
2019-08-15 21:25:04 -0500

    Basic excel code

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
 
 namespace ExpenseList
 {
@@ -10,6 +11,22 @@ namespace ExpenseList
     {
         static void Main(string[] args)
         {
+            //Set Variables
+            string path = "E:\\Excel\\Bank Accounts.xlsx";
+            Application excel = new Application();
+
+            try
+            {
+                excel.Quit();
+            }
+            catch (Exception)
+            {
+                excel.Quit();
+                throw;
+            } finally
+            {
+                excel.Quit();
+            }
         }
     }
 }
