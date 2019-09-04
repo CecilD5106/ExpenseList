c50b99604b1365d19873e5e2ce00673b6f3deae7
Cecil Davis <cecild5106@hotmail.com>
2019-09-03 21:38:37 -0500

    Updated usings

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
 
 namespace ExpenseList
 {
